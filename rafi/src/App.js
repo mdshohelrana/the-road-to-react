6f174d4781dadab138d3c7a743ec096b703b9dbf
Rafiul Isalam <[shohel.rana@mymedicalhub.com]>
2020-06-24 22:25:29 +0600

    Practice: props, default props, typechecking props

--- rafi/src/App.js
+++ rafi/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import ReactHook from "./Day-1-Practice/ReactHook";
-import ClassComponent from "./Day-1-Practice/ClassComponent";
-import AttrAndProps from "./Day-2-Practice/Attr_And_Props";
-import BaseComponent from "./Day-2-Practice/BaseComponent";
-import NestedComponent from "./Day-2-Practice/NestedComponent";
-import UseOfMap from "./Day-2-Practice/UseOfMap";
+import ReactHook from "./Day1/ReactHook";
+import ClassComponent from "./Day1/ClassComponent";
+import AttrAndProps from "./Day2/Attr_And_Props";
+import BaseComponent from "./Day2/BaseComponent";
+import NestedComponent from "./Day2/NestedComponent";
+import UseOfMap from "./Day2/UseOfMap";
+import PropsWithClassContent from "./Day3/PropsWithClassContent";
+import PropsWithFunctionComponent from "./Day3/PropsWithFunctionComponent";
+import DefaultProps from "./Day3/DefaultProps";
 
 function App() {
   return (
@@ -16,6 +19,9 @@ function App() {
       <AttrAndProps attr="This is an attribute and you can access me by props"/>
       <BaseComponent/>
       <UseOfMap/>
+      <PropsWithClassContent msg='Getting Message with props in class content'/>
+      <PropsWithFunctionComponent msg='Getting Message with props in function content'/>
+      <DefaultProps />
     </div>
 
   );
---
+++ rafi/src/Day3/PropsWithFunctionComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PropsWithFunctionComponent = (props) => {
+    return (
+        <div>
+            <h6>
+                Hello Props. {props.msg}
+            </h6>
+        </div>
+    );
+};
+
+export default PropsWithFunctionComponent;
